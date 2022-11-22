36abefcab10cd5dc34cefcb70a4e0ef213705567
A.nassstay <113837511+Anassstay@users.noreply.github.com>
2022-11-22 17:37:52 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -14,14 +14,14 @@ const profileInfo = document.querySelector('.profile__subtitle');
 
 // Открыть попап
 const openPopup = function () {
-    popupElement.classList.add('.popup__opened');
+    popupElement.classList.add('popup__opened');
     nameInput.value = profileName.textContent;
     infoInput.value = profileInfo.textContent;
 }
 
 // Закрыть попап
 const closePopup = function () {
-    popupElement.classList.remove('.popup__opened');
+    popupElement.classList.remove('popup__opened');
 }
 
 // Закрыть при клике за пределами попапа
