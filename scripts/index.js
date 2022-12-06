243d357af24fa84723349e83c76a6b08ecb5a10f
Anastay <Anastasia.stay@yandex.ru>
2022-12-06 21:01:18 +0300

    cancel lag

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@
 // Все попапы
 const popup = document.querySelector('.popup');
 
-// Окно закрывающее попап
+// Окно закрывающее попап редактирования
 const popupCloseButtonElement = popup.querySelector('.popup__close-button');
 
 // Попап редактирования
@@ -38,7 +38,7 @@ const cardsInputImage = document.querySelector('.popup__input_add_link');
 // div попапа добавления карточек
 const formElementAdd = popupElementAdd.querySelector('.popup__content_add');
 
-// Окно закрывающее все попапы
+// Окно закрывающее опап добавления
 const popupCloseButtonElementAdd = popupElementAdd.querySelector('.popup__close-button');
 
 // Кнопка открывающее попап редактирования
@@ -59,32 +59,28 @@ const popupCloseButtonElementPhoto = popupContainerPhoto.querySelector('.popup__
 const cardsElements = document.querySelector('.elements');
 const cardsTemplate = document.querySelector('#cards-template').content;
 
-// Общая функция - открыть попап
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
+// Открыть попап редактирования инф, пыталась реализовать через общую функцию, но попап не открывается
+const openPopupEdit = function () { 
+  popupElementEdit.classList.add('popup_opened');
+   // Из страницы в профиле подставить значение в форму попапа редактирования
+  nameInput.value = profileName.textContent; 
+  infoInput.value = profileInfo.textContent; 
 }
 
-// Общая функция - закрыть попап
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
+// Закрыть попап редактирования инф, пыталась реализовать через общую функцию, но попап не открывается
+const closePopupEdit = function () { 
+  popupElementEdit.classList.remove('popup_opened');
+} 
 
-// popupCloseButtonElementAdd.addEventListener('click', closePopup);
+// Общая функция - открыть попап . не работает
+// function openPopup(popup) {
+//   popup.classList.add('popup_opened');
+// }
 
-// Открыть попап редактирования инф
-const openPopupEdit = function () {
-  openPopup(popupElementEdit);
-  // Из страницы в профиле подставить значение в форму попапа редактирования
-  nameInput.value = profileName.textContent;
-  infoInput.value = profileInfo.textContent;
-}
-//Регистрируем обработчик событий по клику для открытия попапа редактирования
-popupOpenButtonElement.addEventListener('click', openPopupEdit);
-
-// Закрыть попап редактирования
-const closePopupEdit = function () {
-  closePopup(popupElementEdit);
-}
+// Общая функция - закрыть попап . не работает
+// function closePopup(popup) {
+//   popup.classList.remove('popup_opened');
+// }
 
 // // Закрыть при клике за пределами попапа
 // const closePopupByClickOnOverlay = function (event) {
@@ -100,25 +96,25 @@ function formSubmitHandler(event) {
   // Из попапа редактирования подставить значения в профиль
   profileName.textContent = nameInput.value;
   profileInfo.textContent = infoInput.value;
-  closePopup(popupElementEdit);
+  closePopupEdit();
 }
 
 //Регистрируем обработчики событий по клику
-popupElementEdit.addEventListener('click', openPopupEdit);
-popupElementEdit.addEventListener('click', closePopupEdit);
 // popupElementEdit.addEventListener('click', closePopupByClickOnOverlay);
+popupOpenButtonElement.addEventListener('click', openPopupEdit); 
+popupCloseButtonElement.addEventListener('click', closePopupEdit); 
 formElement.addEventListener('submit', formSubmitHandler);
 
 /////////////////////////////////////////////////////////////////////////////////
 
-// Открыть попап добавления
-const openPopupAdd = function () {
-  openPopup(popupElementAdd);
+// Открыть попап добавления, пыталась реализовать через общую функцию, но попап не открывается
+const openPopupAdd = function () { 
+  popupElementAdd.classList.add('popup_opened');
 }
 
-// Закрыть попап добавления
+// Закрыть попап добавления, пыталась реализовать через общую функцию, но попап не открывается
 const closePopupAdd = function () {
-  closePopup(popupElementAdd);
+  popupElementAdd.classList.remove('popup_opened');
 }
 
 // // Закрыть при клике за пределами попапа
@@ -136,23 +132,22 @@ function addFormSubmitHandler(event) {
   // Очистить форму
   nameInput.value = ' ';
   infoInput.value = ' ';
-  closePopup(popupElementAdd);
+  closePopupAdd();
 }
 
 //Регистрируем обработчики событий по клику
 // popupElementAdd.addEventListener('click', closePopupAddByClickOnOverlay);
-// popupOpenButtonElementAdd.addEventListener('click', addFormSubmitHandler);
 popupOpenButtonElementAdd.addEventListener('click', openPopupAdd);
 popupCloseButtonElementAdd.addEventListener('click', closePopupAdd);
 formElementAdd.addEventListener('submit', addFormSubmitHandler);
 
-// Открыть попап
-popupOpenButtonElementAdd.addEventListener('click', () => {
-  // Очистить форму
-  cardsInputImage.value = ' ';
-  cardsInputTitle.value = ' ';
-  openPopup(openPopupAdd);
-})
+// // Открыть попап
+// popupOpenButtonElementAdd.addEventListener('click', () => {
+//   // Очистить форму
+//   // cardsInputImage.value = ' ';
+//   // cardsInputTitle.value = ' ';
+//   openPopupAdd();
+// })
 
 // Открыть попап открытия фото, пыталась реализовать через общую функцию, но попап не открывается
 const openPopupPhoto = function (popup) {
@@ -216,11 +211,9 @@ function createElement(cardsTitleValue, cardsImageValue) {
 
   //  Обработчик событий с функцией открытия фото
   cards.querySelector('.cards__image').addEventListener('click', function () {
-    // openPopup(openPopupPhoto);
     openPopupPhoto();
     openImage.src = cardsImageValue;
     openImageText.textContent = cardsTitleValue;
-    // openImageText = cardsTitleValue;
   });
 
   return cards;
@@ -232,7 +225,7 @@ function handleCardsFormSubmit(event) {
   // Очищаем форму
   cardsInputImage.value = ' ';
   cardsInputTitle.value = ' ';
-  closePopup(popupElementAdd);
+  closePopupAdd();
 }
 
 //Регистрируем обработчик событий
